c559c69d90533017aef0c3005d8ad47a8f3980cd
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-09-29 21:24:15 +0500

    ahammammmmm

---
+++ app.js
@@ -0,0 +1,17 @@
+// var par = document.getElementsByTagName('p');
+//   for (var i = 0; i < par.length; i++) {
+//       par[i].style.fontFamily = "Verdana, Geneva, sans-serif";
+//       par[i].style.color="blue"
+//   }
+//   var textInMiddleParagraph = par[i].innerHTML;
+//   console.log(textInMiddleParagraph);
+
+// var pics = document.getElementsByTagName("img");
+// console.log(pics);
+
+// var t = document.getElementById("table9");
+// var cells = t.getElementsByTagName("td");
+// for (var i = 0; i < cells.length; i++) {
+//     cells[i].style.backgroundColor = "pink";
+// }
+
